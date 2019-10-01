1c9b817e3411a73804d4c06a77da2e6f17613fdc
Seif <seif.dorani@gmail.com>
2019-10-01 01:08:10 -0700

    telling monggose we want to create a new collection called users

--- server/models/User.js
+++ server/models/User.js
@@ -2,3 +2,11 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose
 //const Schema = mongoose.Schema;
+
+
+const UserSchema = new Schema({
+  googleId: String
+});
+
+
+mongoose.model('users', UserSchema);
