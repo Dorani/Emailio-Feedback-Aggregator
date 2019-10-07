deffc7ddc369f1fbf8d57b1339c07de7c1930e2a
Seif <seif.dorani@gmail.com>
2019-10-06 18:23:16 -0700

    atlas connected successfully, data is now hosted on remote server, it works.

--- server/index.js
+++ server/index.js
@@ -2,12 +2,14 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
+const keys = require("./config/keys");
+
 require('./models/User')
 require('./services/passport');
 require('./routes/authRoutes')(app);
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/emaily");
-
+//mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/emaily");
+mongoose.connect(keys.mongoURI);
 const PORT = process.env.PORT || 5000;
 
 
