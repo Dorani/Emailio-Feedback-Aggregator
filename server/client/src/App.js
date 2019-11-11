d92a0eb4264dab4245ad8f3f515be765f600c460
Seif <seif.dorani@gmail.com>
2019-11-10 23:30:52 -0800

    using the proxy module with a method for routing depending on env

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -18,7 +18,7 @@ function App() {
         >
           Learn React
         </a>
-        <a href="http://localhost:5000/auth/google">Sign In with Google</a>
+        <a href="/auth/google">Sign In with Google</a>
       </header>
     </div>
   );
