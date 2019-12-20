e08e475452b47a6f8961af19e4d32e1d859c2dee
Seif <seif.dorani@gmail.com>
2019-12-19 22:31:41 -0500

    importing provider, createStore and applyMiddleware from react-redux and redux and created a store instance with a single dummy reducer to stand in, its an empty array, the second argument will just be an empty obj, the last arg is the applyMiddleware invoked

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware} from 'redux';
+
 
 import App from './components/App'
+
+const store = createStore(() => [], {}, applyMiddleware())
 
 ReactDom.render( <App />, document.querySelector('#root'));
