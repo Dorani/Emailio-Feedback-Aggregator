21d7b64256226dc7a452eb2bbfa16176ea229e14
Seif <seif.dorani@gmail.com>
2019-12-23 22:04:19 -0500

    first route to specifify the connection between the route a user visit and the component to show on the screen

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -14,7 +14,11 @@ const Landing = () => <h2>Landing</h2>
 const App = () => {
   return (
     <div>
-      Hi there!
+      <BrowserRouter>
+        <div>
+          <Route path="/" component={Landing} />
+        </div>
+      </BrowserRouter>
     </div>
   );
 };
