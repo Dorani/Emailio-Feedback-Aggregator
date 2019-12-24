f0daecd972e913f876c270932d04e21eefa068e9
Seif <seif.dorani@gmail.com>
2019-12-23 21:57:54 -0500

    set up with react router dom, with 2 helpers from the lib and got 4 dummy components

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,4 +1,14 @@
 import React from 'react';
+import { BrowserRouter, Route} from 'react-router-dom'
+
+
+const Header = () => <h2>Header</h2>
+const Dashboard = () => <h2>Dashboard</h2>
+const SurveyNew = () => <h2>SurveryNew</h2>
+const Landing = () => <h2>Landing</h2>
+
+
+
 
 
 const App = () => {
