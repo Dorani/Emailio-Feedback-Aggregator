f538517f21b5f1fb3024ae3b2e83cf5a36c36fc9
Seif <seif.dorani@gmail.com>
2019-12-29 15:15:17 -0500

    own header component, basic, created, imported into app, works

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { BrowserRouter, Route} from 'react-router-dom'
+import { BrowserRouter, Route} from 'react-router-dom';
 
 
-const Header = () => <h2>Header</h2>
+import Header from './Header';
+
+
 const Dashboard = () => <h2>Dashboard</h2>
 const SurveyNew = () => <h2>SurveryNew</h2>
 const Landing = () => <h2>Landing</h2>
