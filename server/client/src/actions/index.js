c21aa51ea55b6fcd9b7b1ebd9509db6bf2305022
Seif <seif.dorani@gmail.com>
2019-12-30 12:49:04 -0500

    whenever the action creator is called, it will return a function, redux thunk will see we did this and automatically call it with a dispatch, we then make our request, we wait till we get a response and the promise is resolved, only after this all takes place will we dispatch our action

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -3,5 +3,10 @@ import { FETCH_USER } from './types'
 
 
 const fetchUser = () => {
-  axios.get('/api/current_user');
+
+  return function(dispatch) {
+    axios.get('/api/current_user')
+      .then( res => dispatch({type: FETCH_USER, payload: res}));
+  };
+
 };
