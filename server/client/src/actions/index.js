ce0d837a79c45e155a3a52c4cb1d1ea87c862115
Seif <seif.dorani@gmail.com>
2019-12-29 19:56:37 -0500

    In the last section we started putting together our fetch user action creator which is intended to make
    a request to our back and API and then somehow communicate to our authentic author douceur whether or
    
    not the user is currently signed in.

---
+++ server/client/src/actions/index.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+import { FETCH_USER } from './types'
+
+
+const fetchUser = () => {
+  axios.get('/api/current_user');
+};
--- server/client/src/index.js
+++ server/client/src/index.js
@@ -3,12 +3,15 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware} from 'redux';
+import reduxThunk from 'redux-thunk';
 
 
 import App from './components/App';
 import reducers from './reducers'
 
-const store = createStore(reducers, {}, applyMiddleware())
+
+
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk))
 
 ReactDom.render(
   <Provider store={store}><App /></Provider>,
