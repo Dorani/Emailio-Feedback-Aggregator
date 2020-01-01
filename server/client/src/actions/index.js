994b463c88288ffc052b8324a280b6b337e0315a
Seif <seif.dorani@gmail.com>
2019-12-31 23:47:33 -0500

    async await and es2018 syntax

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -2,10 +2,17 @@ import axios from 'axios';
 import { FETCH_USER } from './types'
 
 
-export const fetchUser = () => {
-  return function(dispatch) {
-    axios
-      .get('/api/current_user')
-      .then( res => dispatch({type: FETCH_USER, payload: res}));
+// export const fetchUser = () => {
+//   return function(dispatch) {
+//     axios
+//       .get('/api/current_user')
+//       .then( res => dispatch({type: FETCH_USER, payload: res}));
+//   };
+// };
+
+//Refactor
+export const fetchUser = () =>
+  async (dispatch) => {
+    const res = await axios.get('/api/current_user');
+      dispatch({type: FETCH_USER, payload: res});
   };
-};
