56f02c89a5a31d73621b71ac58a0f87d5e0830f8
Seif <seif.dorani@gmail.com>
2020-01-01 19:53:49 -0500

    landing page has a jumbotron

--- server/client/src/components/Landing.js
+++ server/client/src/components/Landing.js
@@ -1,15 +1,43 @@
-import React from 'react';
+// import React from 'react';
+//
+//
+// const Landing = () => {
+//   return (
+//   <div style={{textAlign: 'center'}}>
+//     <h1>
+//       Emaily!
+//     </h1>
+//     Collect feedback from your users!
+//   </div>
+//   );
+// }
+//
+//
+//
+//
+// export default Landing;
 
 
-const Landing = () => {
-  return (
-  <div style={{textAlign: 'center'}}>
-    <h1>
-      Emaily!
-    </h1>
-    Collect feedback from your users!
-  </div>
-  );
+import React, { Component } from 'react';
+
+
+const jumbotronStyle = {
+  paddingBottom: '150px',
+  backgroundColor: "#e57373",
+  textAlign: "center"
+}
+
+class Landing extends Component {
+  render() {
+    return (
+      <div className="card-panel" style={jumbotronStyle}>
+        <div className="container">
+          <h1>Welcome to Emaily</h1>
+          <p>Here you can send your users surveys to help better your business...</p>
+        </div>
+       </div>
+    );
+  }
 }
 
 export default Landing;
