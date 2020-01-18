f31b9acc95eccc724a5716f7b5486c163689967d
Seif <seif.dorani@gmail.com>
2020-01-17 23:30:06 -0800

    configuration complete for stripe

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -17,3 +17,6 @@ ReactDom.render(
   <Provider store={store}><App /></Provider>,
   document.querySelector('#root')
 );
+
+console.log('stripe key is', process.env.REACT_APP_STRIPE_KEY);
+console.log('our environment is', process.env.NODE_ENV);
