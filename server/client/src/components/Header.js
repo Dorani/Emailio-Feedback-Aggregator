10d6493fe9a3624f73b796a94eabde2cb41ba0a9
Seif <seif.dorani@gmail.com>
2020-01-20 17:33:27 -0800

    created payments class, imported strike react checkout component module, rendered it, with amount in cents, the token aka the event id as well as our cred, exported, imported, going to add it to when the user authenticates and logs in case

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import Payments from './Payments';
 
 
 class Header extends Component {
@@ -15,7 +16,7 @@ class Header extends Component {
         )
 
       default:
-        return <li> <a href="/api/logout">Logout</a>></li>
+        return <li> <a href="/api/logout">Logout</a>></li>;
     }
   }
 
---
+++ server/client/src/components/Payments.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+
+class Payments extents Component {
+  render() {
+    return (
+      <StripeCheckout
+        amount={500}
+        token={token => console.log(token)}
+        stripeKey={process.env.REACT_APP_STRIPE_KEY}
+       />
+    );
+  };
+};
+
+export default Payments;
