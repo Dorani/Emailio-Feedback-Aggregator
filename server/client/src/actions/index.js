a66909a1d5c9e03cbd5c9668273405368d1aa103
Seif <seif.dorani@gmail.com>
2020-01-20 22:23:39 -0800

    action creator to post our token to the back end server, now we need to make sure it gets called whenever we get a token from the stripe checkout form

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -14,3 +14,9 @@ export const fetchUser = () => async (dispatch) => {
     const res = await axios.get('/api/current_user');
       dispatch({type: FETCH_USER, payload: res.data});
   };
+
+
+export const handleToken = (token) => async (dispatch) => {
+  const rest = await axios.post('/api/stripe', token);
+  dispatch({type: FETCH_USER, payload: res.data});
+};
