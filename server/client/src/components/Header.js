23b35116c58c246a309dc23838a99dc1ce07d648
Seif <seif.dorani@gmail.com>
2020-01-21 22:46:53 -0800

    added credits to the front end for each user

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -18,6 +18,9 @@ class Header extends Component {
       default:
         return [
           <li key="1"> <Payments /> </li>,
+          <li key="3" style={{ margin: '0 10px'}}>
+            Credits: {this.props.auth.credits}
+          </li>,
           <li key="2"> <a href="/api/logout">Logout</a>></li>
         ]
     }
