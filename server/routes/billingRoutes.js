2154a58f5616a7e941f33a45e35aa5414fed3928
Seif <seif.dorani@gmail.com>
2020-01-21 22:00:10 -0800

    used stripe lib to create a new charge, this will make a req to the stripe api, it will tell it we want to finilize this transaction and charged 500 cents

--- server/routes/billingRoutes.js
+++ server/routes/billingRoutes.js
@@ -4,6 +4,11 @@ const stripe = require('stripe')(keys.stripeSecretKey);
 
 module.exports = app => {
   app.post('/api/stripe', (req,res) => {
-    console.log(req.body)
+    stripe.charges.create({
+      amount: 500,
+      currency: 'usd',
+      description: '$5 for 5 Credits!',
+      source: req.body.id
+    })
   });
 };
