ebe5012237942cb3bfd09ab19db20bff16b8bcf0
Seif <seif.dorani@gmail.com>
2020-02-02 22:53:39 -0800

    edits

--- server/models/Survey.js
+++ server/models/Survey.js
@@ -10,12 +10,17 @@ const SurveySchema = new Schema({
   recipients: [recipientSchema],
   yes: { type: Number, default: 0},
   no: { type: Number, default: 0},
-  //every survey will belong to a particular user
-  //the reference we are making this to, is the user collection
-  //this is a relationship field, so we use an underscore, good convention
   _user: { type: Schema.Types.ObjectId, ref: 'User'},
   dateSent: Date,
   lastResponded: Date
 });
 
 mongoose.model('surveys', SurveySchema)
+
+
+
+
+//Notes for line 13:
+//every survey will belong to a particular user
+//the reference we are making this to, is the user collection
+//this is a relationship field, so we use an underscore, good convention
