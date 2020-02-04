0d6da044f1d63004dfa1f16159c953c74d77117d
Seif <seif.dorani@gmail.com>
2020-02-03 21:38:11 -0800

    es7 refactor

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -10,9 +10,9 @@ module.exports = app => {
     const { title, subject, body, recipient } = req.body;
 
     const survey = new Survey({
-      title: title,
-      subject: subject,
-      body: body,
+      title,
+      subject,
+      body,
       recipients: recipients.split(',').map(email => ({ email: email.trim() })),
       _user: req.user.id,
       dateSent: Date.now()
