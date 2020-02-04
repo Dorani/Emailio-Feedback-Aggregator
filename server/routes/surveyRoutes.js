e2a6aba9156e310cc9a653958a2ade83e6ac569b
Seif <seif.dorani@gmail.com>
2020-02-03 21:15:30 -0800

    if anybody makes a post request to api surveys, first check that they are logged in then make sure they have enough credits, then finally they can create a survey, which comes next

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -1,9 +1,9 @@
 const requireLogin = require('../middlewares/requireLogin')
-
+const requireCredits = require('.//middlewares/requireCredits')
 
 module.exports = app => {
   //if you are logged in and have credits then you can send out a survey, middlewares
-  app.post('/api/surveys', requireLogin, (req, res) => {
+  app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
 
   });
 };
