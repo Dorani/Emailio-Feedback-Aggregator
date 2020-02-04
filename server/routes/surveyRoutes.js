2c546627354ab19154f6aab6630fffb9cdac6eb3
Seif <seif.dorani@gmail.com>
2020-02-03 21:37:18 -0800

    creation of a new survey inside our route handler, not yet saved to our mongo db

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -1,9 +1,21 @@
-const requireLogin = require('../middlewares/requireLogin')
-const requireCredits = require('.//middlewares/requireCredits')
+const mongoose = require('mongoose');
+const requireLogin = require('../middlewares/requireLogin');
+const requireCredits = require('../middlewares/requireCredits');
+
+const Survey = mongoose.model('surveys');
 
 module.exports = app => {
   //if you are logged in and have credits then you can send out a survey, middlewares
   app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+    const { title, subject, body, recipient } = req.body;
 
+    const survey = new Survey({
+      title: title,
+      subject: subject,
+      body: body,
+      recipients: recipients.split(',').map(email => ({ email: email.trim() })),
+      _user: req.user.id,
+      dateSent: Date.now()
+    });
   });
 };
