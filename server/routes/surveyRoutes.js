e9ccc4cda18cb48dc5f5c87cedc94803106e8e60
Seif <seif.dorani@gmail.com>
2020-02-10 23:40:12 -0800

    after survey is built, we want to use our mailer class to create a new instance of a class, ie an object to represent an email, to customize what gets sent, we pass in arguments to congifure this mailer object

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
+const Mailer = require('../services/Mailer');
 
 const Survey = mongoose.model('surveys');
 
@@ -17,5 +18,7 @@ module.exports = app => {
       _user: req.user.id,
       dateSent: Date.now()
     });
+    //Send an email
+    const mailer = new Mailer(survey, );
   });
 };
--- server/services/Mailer.js
+++ server/services/Mailer.js
@@ -7,3 +7,5 @@ const keys = require('../config/keys');
 class Mailer extends helper.Mail {
 
 }
+
+module.exports = Mailer;
